c94768aa5f1f67c01e37495233a72bb90c34dbfe
arif0718 <arifansari90467@gmail.com>
2025-07-11 16:56:00 +0530

    commited

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
-const Home = ({ name, age }) => {
-    return (
-        <div>
-            <h1>Hello {name}. You are {age} years old. </h1>
-            <p>This is a paragraph</p>
-        </div>
-    )
+import "./Home.css"
+export default function Home({ name, age }) {
+  let id = 3453566;
+  return (
+    <>
+      <h1 style={{ backgroundColor: "orange",color:"blue" }}>
+        Hello {name}. You are {age} years old.
+      </h1>
+      <h2 className="App-Home-Header">Your Student Id is {id}</h2>
+      <p>This is a paragraph</p>
+    </>
+  );
 }
-
-export default Home
